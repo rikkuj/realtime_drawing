554a73810e840d235dababc2d5a1beabb2b3c351
rikkuj <89080386+rikkuj@users.noreply.github.com>
2021-09-09 13:05:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,550);
+    canvas.position(675,150);
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function draw(){
+    background('#FF0000');
+    fill("#FF69B4");
+    stroke("#FF69B4");
+    square(noseX,noseY,difference);
+
+    document.getElementById("square_side").innerHTML = "Width and Hieght of square will be = " + difference + "px";
+}
+
+function modelLoaded(){
+    console.log('Pose net is initialised');
+}
+
+function gotPoses(results){
+if(results.length > 0){
+    console.log(results)
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX + "noseY = " + noseY);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+    console.log("leftWristx = " + leftWristX + "rightWrightx = " + rightWristX + "difference = " + difference);
+}
+}
